49de7e003640638099a7071604c8b4c777cf53a7
harleysherman <harsatr11@icloud.com>
2024-01-19 15:09:58 -0600

    deleted readme to retry

--- index.js
+++ index.js
@@ -1,74 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const {writeFile} = require('fs').promises;
-const generateMarkdown = require('./utils/generateMarkdown');
-
-const generateReadme = ({projectName, buildReason, solution, learn, installation, useCases, screenshotLink, credit, license, badge, features, gitHubUserName}) =>
-`# <${projectName}>
-
-## Description
-
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- Motivation: ${motivation}
-- Reason for build: ${buildReason}
-- Solution: ${solution}
-- What I learned: ${learn}
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-
-## Installation
-
-${installation}
-
-## Usage
-
-${useCases}
-
-To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-md
-(need exclamation point here)[alt text](./assest/images/${screenshotLink})
-
-
-## Credits
-
-${credit}
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-## License
-
-${generateMarkdown.renderLicenseLink (license)}
-
-
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-Link EX: My favorite search engine is [Duck Duck Go](https://duckduckgo.com).
-
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-(need exclamation point here)[badmath](${generateMarkdown.renderLicenseBadge(badge)})
-
-## Features
-
-${features}
-
-## How to Contribute
-
-[Contributor Covenant]mailto:${generateMarkdown.renderGitHubLink (gitHubUserName) }`;
+const fs = require('fs').promises;
+const generateReadme = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -146,7 +79,7 @@ const questions = [
     {
         type : 'input',
         message : "Please add your email for contribution.",
-        name : 'contributorInfo',
+        name : 'contributorInfoEmail',
     },
     {
         type : 'input',
@@ -159,11 +92,7 @@ const questions = [
 function init() {
     //prompt for questions in terminal
     inquirer.prompt(questions)
-
-    // TODO: Create a function to write README file
-    console.log(response);
-    //next line has an error for some reason :)
-    .then((response) => writeFile('readme.md', generateReadme(response)))
+    .then((response) => fs.writeFile ('readme.md', generateReadme(response)))
     .then(() => console.log('Successfully wrote to readme.md'))
     .catch((err) => console.error(err));
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,22 +14,23 @@ function renderLicenseBadge(badge) {
 function renderLicenseLink (license) {
   switch (license) {
     case 'MIT':
-      console.log(maths.sum(numOne, numTwo));
+      console.log("This project uses the MIT License.");
+      //license = (https://img.shields.io/badge/${license});
       break;
     case 'Apache 2.0':
-      console.log(maths.difference(numOne, numTwo));
+      console.log("This project uses the Apache 2.0 License.");
       break;
     case 'GPL 3.0':
-      console.log(maths.product(numOne, numTwo));
+      console.log("This project uses the GPL 3.0 License.");
       break;
     case 'BSD 2-Clause':
-      console.log(maths.quotient(numOne, numTwo));
+      console.log("This project uses the BSD 2-Clause License.");
       break;
     case 'BSD 3-Clause':
-      console.log(maths.quotient(numOne, numTwo));
+      console.log("This project uses the BSD 3-Clause License.");
       break;
     case 'BSD 4-Clause':
-      console.log(maths.quotient(numOne, numTwo));
+      console.log("This project uses the BSD 4-Clause License.");
       break;
     default:
       console.log('Check your maths!');
@@ -46,10 +47,67 @@ function renderGitHubLink (gitHubUserName) {
 
 // TODO: Create a function to generate markdown for README
 //returning a template literal in index.js generateMarkdown
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateReadme = ({projectName, motivation, buildReason, solution, learn, installation, useCases, screenshotLink, credit, license, badge, features, gitHubUserName, contributorInfoEmail, testCases}) => `# <${projectName}>
 
-`;
-}
+## Description
 
-module.exports = generateMarkdown;
+A short description explaining the what, why, and how of your project:
+
+- Motivation: ${motivation}
+- Reason for build: ${buildReason}
+- Solution: ${solution}
+- What I learned: ${learn}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${useCases}
+
+[screenshot](./assest/images/${screenshotLink})
+
+
+## Credits
+
+${credit}
+
+## License
+
+![${license}](${renderLicenseLink (license)})
+
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+Link EX: My favorite search engine is [Duck Duck Go](https://duckduckgo.com).
+
+## Questions
+
+![${gitHubUserName}](https://www.github.com/${gitHubUserName})
+![${contributorInfoEmail}](mailto:${contributorInfoEmail})
+---
+
+## Badges
+
+![${badge}](${renderLicenseBadge(badge)})
+
+## Features
+
+${features}
+
+## How to Contribute
+
+![${gitHubUserName}](${renderGitHubLink (gitHubUserName)})
+
+## Tests
+
+${testCases}`;
+
+module.exports = generateReadme; //modularization
